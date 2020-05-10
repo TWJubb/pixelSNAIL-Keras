ae5643b770538c511d55c470de7683f1ef2cf4b3
TWJubb <t.w.jubb@gmail.com>
2020-05-10 19:37:51 +0100

    Add docstring to causal mask function

--- pixelsnail_keras/utils.py
+++ pixelsnail_keras/utils.py
@@ -2,12 +2,20 @@ import numpy as np
 import tensorflow as tf
 
 
-def get_causal_mask(canvas_size, rate=1):
+def get_causal_mask(canvas_size):
     """
+    Create a basic mask for causality in the multi-head attention block; the mask (when multiplied by the attention
+    tensor) sets to zero all elements in the "future". The attention tensor is (HW, HW) shape and element (i,j)
+    describes how spatial location i influences spatial location j; therefore the mask will be
 
-    :param canvas_size:
-    :param rate:
-    :return:
+    1 0 0 0 ... 0
+    1 1 0 0 ... 0
+    1 1 1 0 ... 0
+    1 1 1 1 ... 0
+    : : : :     :
+    0 0 0 0 ... 1
+
+    canvas size is H.W (the total number of spatial positions in the attention map); e.g. for a 32x32 image HW=1024
     """
     causal_mask = np.zeros([canvas_size, canvas_size], dtype=np.float32)
     for i in range(canvas_size):
